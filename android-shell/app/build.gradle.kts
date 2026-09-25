af29eedc0df4df22e2725e8acc7c86e257118c22
TIMER-err <zqq2333@outlook.com>
2026-09-25 20:35:54 +0800

    chore: release 1.6.5

--- android-shell/app/build.gradle.kts
+++ android-shell/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "dev.t1m3.qplayer"
         minSdk = 26
         targetSdk = 34
-        versionCode = 80
-        versionName = "1.6.4"
+        versionCode = 81
+        versionName = "1.6.5"
         manifestPlaceholders["appLabel"] = "QPlayer"
     }
 
